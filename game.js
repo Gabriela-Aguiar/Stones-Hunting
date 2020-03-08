ce848085f4ae308eb7c9755b2c9f75d24681aad2
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-03-08 20:49:31 -0300

    character moves

--- game.js
+++ game.js
@@ -1,27 +1,30 @@
 const principalSpace = document.getElementById('principal')
 
-
-
-
 const hero = new Hero({x:100, y:650, width:100, height:100, strength:100, health:100, characterimg:'/assets/Gamoracard.png'})
 
-console.log(hero)
+var requestID = null;
 
 const myGameArea = {
     canvas: document.createElement("canvas"),
+    frames: 0,
+    time: 0,
     start: function() {
         console.log( this.canvas )
         this.canvas.width = 1420;
         this.canvas.height = 770;
-        this.canvas.style.backgroundColor = 'orange'
         this.context = this.canvas.getContext("2d");
         this.character = localStorage.getItem('character')
         principalSpace.appendChild(this.canvas)
+        requestID = window.requestAnimationFrame(updateGameArea)
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
     drawBackground: function(){
-        const img = new Image();
+        const img = new Image()
+        console.log('update');
         img.onload = () => {
-          this.context.drawImage(img, 0, 0, 1420,770);
+            this.context.drawImage(img, 0, 0, 1420,770);
             myGameArea.drawScore()
             myGameArea.drawCharacterName()
             myGameArea.drawThanosName()
@@ -64,7 +67,7 @@ const myGameArea = {
     },
     drawTime: function(){
         this.context.font = "50px Fredoka One";
-        this.context.fillText('0:45', 650, 100);
+        this.context.fillText(this.time + " seconds", 600, 100);
     },
     drawScore: function(){
         this.context.font = "25px Fredoka One";
@@ -76,6 +79,7 @@ const myGameArea = {
         // this.context.fillRect(120,650, 30, 100);
         const img = new Image();
         img.onload = () => {
+            console.log('aca carga img')
             this.context.drawImage(img, hero.x, hero.y, hero.width,hero.height);
         }
         img.src = hero.characterimg;
@@ -93,6 +97,36 @@ const myGameArea = {
 
 
 
+
 myGameArea.start()
 myGameArea.drawBackground()
 
+const time = setInterval(() => {
+    myGameArea.time++ 
+}, 1000);
+
+
+
+function updateGameArea() {
+    console.log('update')
+    //myGameArea.clear();
+    myGameArea.drawBackground()
+    requestID = window.requestAnimationFrame(updateGameArea);
+}
+
+document.addEventListener('keydown', function(event) {
+   if(event.keyCode == 39) {
+        if(hero.x < myGameArea.canvas.width - 100){
+        hero.x = hero.x+20
+       }
+   } else if (event.keyCode == 37){
+       if (hero.x > 0){
+        hero.x = hero.x-20
+       }
+   } else if (event.keyCode == 38){
+       if (hero.y > 0){
+       hero.y = hero.y-20
+       }
+   }
+    console.log('asdasljdhiyasgdliygaiysdlgasgd')
+  });
