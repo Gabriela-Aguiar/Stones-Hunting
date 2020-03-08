adc02837a214b1d313b7d9cc9842a36dc0c9e0f2
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-03-07 22:43:51 -0300

    select random

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const characters = ['rocket', 'quill', 'gamora', 'drax']
+
 const step = (actual, next) => {
     document.getElementById(actual).classList.add("no-show")
     document.getElementById(next).classList.remove("no-show")
@@ -5,5 +7,43 @@ const step = (actual, next) => {
 
 const character = (character) => {
  console.log(character)
+ localStorage.setItem('character',character)
  step('second','third')
+ characterSelected()
+}
+
+const random = (random) => {
+    let finalCharacter = '';
+    let count = 0;
+    for (i = 0; i < characters.length; i++) {
+        document.getElementById(characters[i]).removeAttribute("onClick")
+        document.getElementById(characters[i]).classList.remove("person")
+    }
+    const time = setInterval(() => { 
+        if (count < 25) {
+        const randomCharacter = Math.floor(Math.random()*characters.length);
+        for (i = 0; i < characters.length; i++) {
+            document.getElementById(characters[i]).style.backgroundColor = "rgba(0,0,0,0.7)"
+        }
+        const elem = document.getElementById(characters[randomCharacter])
+        elem.style.backgroundColor = "rgba(0,0,0,0)"
+        count++
+        finalCharacter = characters[randomCharacter];
+      } else {
+          clearInterval(time)
+          setTimeout(() => {
+              character(finalCharacter);
+          }, 2000)
+      }
+    }, 100);
+}
+
+const characterSelected = () => {
+    const people = localStorage.getItem('character')
+    for( let i = 0; i < characters.length; i++) {
+        document.getElementById(characters[i] + "-photo").classList.remove('no-show')
+        if( people != characters[i] ){
+            document.getElementById(characters[i] + "-photo").classList.add('no-show')
+        }
+    }
 }
